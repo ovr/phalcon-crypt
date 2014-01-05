d066c79a784587f1cecab4ef479ee69c813d62fd
Vladimir Kolesnikov <vladimir@free-sevastopol.com>
2014-01-05 17:51:12 +0200

    Try to run tests under valgrind

--- .travis/run_php_tests.sh
+++ .travis/run_php_tests.sh
@@ -10,5 +10,15 @@ for i in $EXT_DIR/tests/*.log; do
 	echo "====== $i ======";
 	cat "$i";
 done
-[ -n "$(echo $EXT_DIR/tests/*.log)" ] && exit 1
-exit 0
+
+[ -n "$(echo $EXT_DIR/tests/*.log)" ] && EXIT_STATUS=1 || EXIT_STATUS=0
+
+sed -i 's!run-tests.php!run-tests.php -m!g' "$EXT_DIR/Makefile"
+export USE_ZEND_ALLOC=0
+make -C "$EXT_DIR" test
+for i in $EXT_DIR/tests/*.log $EXT_DIR/tests/*.mem; do
+	echo "====== $i ======";
+	cat "$i";
+done
+
+exit $EXIT_STATUS
